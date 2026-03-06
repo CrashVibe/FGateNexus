7a56660b036595442d662ed94aa81d086b007e37
MrlingXD <90316914+wling-art@users.noreply.github.com>
2026-03-07 07:10:15 +0800

    🐛 fix(db.client): 添加数据库实例创建的 PRAGMA 设置 (#56)

--- server/db/client.ts
+++ server/db/client.ts
@@ -9,6 +9,10 @@ const createDatabaseInstance = (): BunSQLiteDatabase<typeof schema> => {
   const sqlite = new Database("sqlite.db");
   sqlite.run("PRAGMA journal_mode = WAL;");
   sqlite.run("PRAGMA synchronous = NORMAL;");
+  sqlite.run("PRAGMA foreign_keys = ON;");
+  sqlite.run("PRAGMA busy_timeout = 5000;");
+  sqlite.run("PRAGMA cache_size = -64000;");
+  sqlite.run("PRAGMA temp_store = MEMORY;");
   return drizzle(sqlite, { schema });
 };
 
