9d020065fddcd057ec6b0043fdec4612adb32a67
MrlingXD <90316914+wling-art@users.noreply.github.com>
2026-05-04 10:03:18 +0800

    :recycle: refactor: 改进退出逻辑

--- server/plugins/1.graceful-shutdown.ts
+++ server/plugins/1.graceful-shutdown.ts
@@ -1,6 +1,33 @@
 import readline from "node:readline";
 
+const forceExit = () => {
+  logger.warn("好好好，不收拾了，跑路辽");
+  process.exit(0);
+};
+
 export default defineNitroPlugin((nitro) => {
+  let isClosing = false;
+  const gracefulShutdown = async () => {
+    if (isClosing) {
+      return;
+    }
+    isClosing = true;
+    logger.info("等我收拾一下嗷...");
+    await nitro.hooks.callHook("close");
+    logger.info("收拾好啦，祝你有好的一天～");
+    process.exit(0);
+  };
+
+  process.on("SIGTERM", () => {
+    void gracefulShutdown();
+  });
+  process.on("SIGINT", () => {
+    void gracefulShutdown();
+  });
+  process.on("SIGQUIT", () => {
+    void gracefulShutdown();
+  });
+
   const canUseRawMode = typeof process.stdin.setRawMode === "function";
   if (!process.stdin.isTTY || !canUseRawMode) {
     return;
@@ -14,13 +41,9 @@ export default defineNitroPlugin((nitro) => {
   process.stdin.on("keypress", (_str, key) => {
     void (async () => {
       if (key.sequence === "\u0003") {
-        logger.info("等我收拾一下嗷...");
-        await nitro.hooks.callHook("close");
-        logger.info("收拾好啦，祝你有好的一天～");
-        process.exit(0);
+        await gracefulShutdown();
       } else if (key.ctrl && key.name === "k") {
-        logger.warn("好好好，不收拾了，跑路辽");
-        process.exit(0);
+        forceExit();
       }
     })();
   });
